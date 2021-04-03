4da629c3d4db1a6482e2541d06af1706ff62a041
郭慕瑜 <65473106+Lance20001003@users.noreply.github.com>
2021-04-03 22:45:50 +0800

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,21 @@
+# encoding:utf-8
+# FileName: __init__.py
+# Author:   Lance
+# email:    742240117@qq.com
+# Date:     2021/4/3 16:33
+# Description: 爬取基金季度报告
+
+import pandas as pd
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# 显示所有列
+pd.set_option('display.max_columns', None)
+# 显示所有行
+# pd.set_option('display.max_rows', None)
+
+
+if __name__ == '__main__':
+    pass
